ac0da298f2dc4aa37fa19be3759282a9476484a0
McCabe Northup <mccabenorthup@gmail.com>
2020-01-30 19:40:38 -0700

    updated burger model to be passed through controler

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+
+    // cat.all(cb) pulled from burgers_controller.js
+    all: function(cb) {
+        //orm.all(tableName, cb)
+        orm.all("burgers", function(res){
+            cd(res);
+        });
+    },
+    // cat.create(["name", "devoured"], ["Blue Ribbon Burger", "1"], cb)
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+                cd(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res)
+        });
+    }
+
+    // possibly put a delete here for a true CRUD file
+};
+
+module.exports = burger;
