3d36b93786cd8e59fd383c4f2cb31ef6a4e219cf
McCabe Northup <mccabenorthup@gmail.com>
2020-02-01 22:23:18 -0700

    added JAWS_DB credentials

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(provess.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.password,
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
