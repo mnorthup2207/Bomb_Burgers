fe960cfad64e5e89cf34c405bb8062ec4af7b2d1
McCabe Northup <mccabenorthup@gmail.com>
2020-02-01 23:20:56 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 var connection;
 
-if(provess.env.JAWSBD_URL) {
+if(process.env.JAWSBD_URL) {
     connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
  connection = mysql.createConnection({
